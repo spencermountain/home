68fd4188225570686870f5f9cf3a1f59a0e91190
spencer kelly <spencerkelly86@gmail.com>
2026-09-20 20:18:21 -0400

    lint working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,13 @@
-5
+// eslint.config.mjs
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  // Custom rule overrides
+  {
+    rules: {
+      'vue/require-default-prop': 'off',
+      'vue/html-self-closing': 'off',
+      'vue/first-attribute-linebreak': 'off'
+    }
+  }
+)
